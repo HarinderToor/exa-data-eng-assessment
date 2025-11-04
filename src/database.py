c6390d81cb70adfc498b3d33c76136f0e51a477e
harinder.toor <harindertoor87@gmail.com>
2025-11-04 13:52:31 +0000

    Modularize Database env variable handling

--- src/database.py
+++ src/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///:memory:")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
